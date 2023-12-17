bfa3c0552b83e7058cc033736114be19da14a00a
Dinesh2811 <dineshk281198@gmail.com>
2023-12-17 05:37:02 +0530

    19-12-23 5:36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,26 +9,20 @@ android {
     namespace = "com.dinesh.android"
     compileSdk = 34
 
-    val data: String? = System.getenv("MYAPP_UAT_STORE_FILE")
-    var file: Any = file("${rootProject.projectDir}/dinesh28-release-key.jks")
-    if (!data.isNullOrEmpty()) {
-        file = System.getenv("MYAPP_RELEASE_STORE_FILE")
-    }
-
-    signingConfigs {
-        create("release") {
-            storeFile = file(System.getenv("MYAPP_RELEASE_STORE_FILE")?: "${rootProject.projectDir}/dinesh28-release-key.jks")
-            storePassword = System.getenv("MYAPP_RELEASE_STORE_PASSWORD") ?: ""
-            keyAlias = System.getenv("MYAPP_RELEASE_KEY_ALIAS") ?: "dinesh28-key-alias"
-            keyPassword = System.getenv("MYAPP_RELEASE_KEY_PASSWORD") ?: ""
-        }
-        getByName("debug") {
-            storeFile = file(System.getenv("MYAPP_UAT_STORE_FILE")?: "${rootProject.projectDir}/dinesh28-release-key.jks")
-            storePassword = System.getenv("MYAPP_UAT_STORE_PASSWORD") ?: "dinesh28Android"
-            keyAlias = System.getenv("MYAPP_UAT_KEY_ALIAS") ?: "dinesh28-key-alias"
-            keyPassword = System.getenv("MYAPP_UAT_KEY_PASSWORD") ?: "dinesh28Android"
-        }
-    }
+//    signingConfigs {
+//        create("release") {
+//            storeFile = file(System.getenv("MYAPP_RELEASE_STORE_FILE")?: "${rootProject.projectDir}/dinesh28-release-key.jks")
+//            storePassword = System.getenv("MYAPP_RELEASE_STORE_PASSWORD") ?: "dinesh28Android"
+//            keyAlias = System.getenv("MYAPP_RELEASE_KEY_ALIAS") ?: "dinesh28-key-alias"
+//            keyPassword = System.getenv("MYAPP_RELEASE_KEY_PASSWORD") ?: "dinesh28Android"
+//        }
+//        getByName("debug") {
+//            storeFile = file(System.getenv("MYAPP_UAT_STORE_FILE")?: "${rootProject.projectDir}/dinesh28-release-key.jks")
+//            storePassword = System.getenv("MYAPP_UAT_STORE_PASSWORD") ?: "dinesh28Android"
+//            keyAlias = System.getenv("MYAPP_UAT_KEY_ALIAS") ?: "dinesh28-key-alias"
+//            keyPassword = System.getenv("MYAPP_UAT_KEY_PASSWORD") ?: "dinesh28Android"
+//        }
+//    }
 
 //    signingConfigs {
 //        create("release"){
@@ -70,7 +64,7 @@ android {
             }
         }
         debug {
-            signingConfig = signingConfigs.getByName("debug")
+//            signingConfig = signingConfigs.getByName("debug")
 //            applicationIdSuffix = ".debug"
             isMinifyEnabled = false
             isShrinkResources = false
@@ -97,49 +91,41 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.6"
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-    signingConfigs {
-        getByName("debug") {
-//            storeFile = file("../keystore/debug.keystore")
-//            storePassword = "android_storePassword"
-//            keyAlias = "android_debug_key"
-//            keyPassword = "android_keyPassword"
-        }
-    }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.compose.ui:ui:1.5.0")
-    implementation("androidx.compose.ui:ui-graphics:1.5.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.ui:ui-graphics:1.5.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation("com.google.android.material:material:1.11.0-alpha01")
-    implementation("androidx.compose.material:material:1.5.0")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha05")
-    implementation("androidx.compose.material3:material3-android:1.2.0-alpha05")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.compose.material:material:1.5.4")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha12")
+    implementation("androidx.compose.material3:material3-android:1.2.0-alpha12")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.4")
 
-    implementation("androidx.compose:compose-bom:2023.08.00")
-    androidTestImplementation("androidx.compose:compose-bom:2023.08.00")
+    implementation("androidx.compose:compose-bom:2023.10.01")
+    androidTestImplementation("androidx.compose:compose-bom:2023.10.01")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-core:2.19.0")
+    testImplementation("org.mockito:mockito-core:5.3.1")
 
 }
